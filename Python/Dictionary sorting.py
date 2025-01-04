934bd25ffa99ce780b59bc366729e8ee950edb7e
Akarsh E.R <153263317+Akarsher@users.noreply.github.com>
2025-01-04 21:58:48 +0530

    Create Dictionary sorting.py
    Program to  sort dictionary in ascending and descending order.

---
+++ Python/Dictionary sorting.py
@@ -0,0 +1,10 @@
+d = {  
+'apple': 10,  
+'banana': 15,  
+'cherry': 2,  
+'date': 5,  
+} 
+#ascending 
+print("Ascending Order : ",dict(sorted(d.items()))) 
+#descending 
+print("Descending Order : ",dict(sorted(d.items(),reverse=True))) 
