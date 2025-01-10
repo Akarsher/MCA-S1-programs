9658ab4e43502bd53fae83dc2a55b6b7f2ac0bfb
Akarsh E.R <153263317+Akarsher@users.noreply.github.com>
2025-01-10 13:03:45 +0530

    Update 18.py

---
+++ Python/CO-1/18.py
@@ -0,0 +1,20 @@
+#Merge two dictionaries.
+
+d1={
+	"Apple":101,
+	"Watermelon": 220,
+	"Lemon": 50
+}
+
+d2={
+	"Mango":100,
+	"Bluberry": 140,
+	"Kiwi": 56
+}
+
+print(d1,"\n",d2)
+
+print(d1|d2)
+
+
+print(d1)
